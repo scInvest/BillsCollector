e371cbdfee0e963752a735116283da3427c5c932
Patryk PGR <1992grabek@gmail.com>
2026-05-03 13:43:38 +0200

    add main logic app and project

---
+++ CostAnalizerApp.Tests/Class1Tests.cs
@@ -0,0 +1,17 @@
+namespace CostAnalizerApp.Tests;
+
+[TestClass]
+public class Class1Tests
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        // Arrange
+        var class1 = new Class1();
+
+        // Act
+
+        // Assert
+        Assert.IsNotNull(class1);
+    }
+}
