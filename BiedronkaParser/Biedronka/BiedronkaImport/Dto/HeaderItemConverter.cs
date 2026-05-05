c941d8e7b0f05c59faba83cb54a7214029519375
Patryk PGR <1992grabek@gmail.com>
2026-05-05 22:41:10 +0200

    rename project before stating new integration

--- BiedronkaParser/Biedronka/BiedronkaImport/Dto/HeaderItemConverter.cs
+++ BiedronkaParser/Biedronka/BiedronkaImport/Dto/HeaderItemConverter.cs
@@ -2,7 +2,7 @@ using System;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
-namespace BiedronkaParser.BiedronkaImport.Dto
+namespace Integrations.Biedronka.BiedronkaImport.Dto
 {
     public class HeaderItemConverter : JsonConverter<HeaderItem>
     {
--- BiedronkaParser/Biedronka/BiedronkaImport/Dto/ImageHeader.cs
+++ BiedronkaParser/Biedronka/BiedronkaImport/Dto/ImageHeader.cs
@@ -1,4 +1,4 @@
-namespace BiedronkaParser.BiedronkaImport.Dto
+namespace Integrations.Biedronka.BiedronkaImport.Dto
 {
     public class ImageHeader : HeaderItem
     {
