17d13ec5bda353bd244cbba0f6ea7f303f0df16b
patryk grabski <patryk.grabski@aveva.com>
2026-05-30 19:12:56 +0200

    add view model base

---
+++ WebClient/ViewModels/ViewModelBase.cs
@@ -0,0 +1,163 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Components;
+
+namespace WebClient.ViewModels
+{
+    public interface IRefreshableComponent
+    {
+        void Refresh();
+    }
+
+    // Minimal base ViewModel.
+    // - INotifyPropertyChanged
+    // - Parent / Children
+    // - HasChanges flag
+    public class ViewModelBase : INotifyPropertyChanged, IDisposable
+    {
+        private readonly List<ViewModelBase> _children;
+        // Update batching fields
+        private int _updateNesting;
+        private bool _refreshPending;
+        private Propagate _pendingPropagate;
+
+        [Flags]
+        public enum Propagate
+        {
+            None = 0,
+            This = 1,
+            Children = 2,
+            Parents = 4,
+            Root = 8,
+            WholeTree = 16
+        }
+
+        public ViewModelBase(ComponentBase component, IEnumerable<ViewModelBase>? children = null, ViewModelBase? parent = null)
+        {
+            if (component == null)
+            {
+                throw new ArgumentNullException(nameof(component));
+            }
+            Component = component;
+            _children = children != null ? new List<ViewModelBase>(children) : new List<ViewModelBase>();
+            foreach (var child in _children)
+            {
+                child.Parent = this;
+            }
+            if (parent != null)
+            {
+                Parent = parent;
+            }
+        }
+
+        public ViewModelBase(ComponentBase component, IEnumerable<ViewModelBase> children)
+            : this(component, children, null) { }
+
+        public ViewModelBase(ComponentBase component, ViewModelBase parent)
+            : this(component, null, parent) { }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public ViewModelBase? Parent { get; set; }
+
+        public IReadOnlyList<ViewModelBase> Children => _children;
+
+        public ComponentBase Component { get; }
+
+        public void BeginUpdate()
+        {
+            _updateNesting++;
+        }
+
+        public void EndUpdate()
+        {
+            if (_updateNesting == 0) { return; }
+            _updateNesting--;
+            if (_updateNesting == 0 && _refreshPending)
+            {
+                _refreshPending = false;
+                var propagate = _pendingPropagate;
+                _pendingPropagate = Propagate.None;
+                RefreshInternal(propagate);
+            }
+        }
+
+        private void RefreshInternal()
+        {
+            RefreshInternal(Propagate.This);
+        }
+
+        public void Refresh()
+        {
+            Refresh(Propagate.This);
+        }
+
+        private void RefreshInternal(Propagate propagate)
+        {
+            if (_updateNesting > 0)
+            {
+                _refreshPending = true;
+                _pendingPropagate |= propagate;
+                return;
+            }
+
+            if ((propagate & Propagate.WholeTree) != 0)
+            {
+                var rootNode = this;
+                while (rootNode.Parent != null)
+                {
+                    rootNode = rootNode.Parent;
+                }
+                rootNode.RefreshInternal(Propagate.This | Propagate.Children);
+            }
+
+            if ((propagate & Propagate.Root) != 0)
+            {
+                var root = this;
+                while (root.Parent != null)
+                {
+                    root = root.Parent;
+                }
+                root.RefreshInternal(Propagate.This);
+            }
+
+            if ((propagate & Propagate.This) != 0)
+            {
+                if (Component is IRefreshableComponent refreshable)
+                {
+                    refreshable.Refresh();
+                }
+                else
+                {
+                    throw new InvalidOperationException($"Component does not implement IRefreshableComponent: {Component.GetType().FullName}");
+                }
+            }
+
+            if ((propagate & Propagate.Children) != 0)
+            {
+                foreach (var child in Children)
+                {
+                    child.RefreshInternal(Propagate.This | Propagate.Children);
+                }
+            }
+
+            if ((propagate & Propagate.Parents) != 0)
+            {
+                Parent?.RefreshInternal(Propagate.This | Propagate.Parents);
+            }
+        }
+
+        public void Refresh(Propagate propagate)
+        {
+            RefreshInternal(propagate);
+        }
+
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
---
+++ WebClient/ViewModels/ViewModelBaseTests.cs
@@ -0,0 +1,71 @@
+//using System;
+//using System.Collections.Generic;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using Microsoft.AspNetCore.Components;
+//using WebClient.ViewModels;
+
+//namespace WebClient.ViewModels.Tests
+//{
+//    [TestClass]
+//    public class ViewModelBaseTests
+//    {
+//        private class TestComponent : ComponentBase, IRefreshableComponent
+//        {
+//            public int RefreshCount { get; private set; }
+//            public void Refresh() { RefreshCount++; }
+//        }
+
+//        private class TestViewModel : ViewModelBase
+//        {
+//            public TestViewModel(ComponentBase component) : base(component) { }
+//            public TestViewModel(ComponentBase component, IEnumerable<ViewModelBase> children, ViewModelBase parent = null) : base(component, children, parent) { }
+//        }
+
+//        [TestMethod]
+//        public void Refresh_This_OnlyThisComponentRefreshed()
+//        {
+//            var comp = new TestComponent();
+//            var vm = new TestViewModel(comp);
+//            vm.Refresh(ViewModelBase.Propagate.This);
+//            Assert.AreEqual(1, comp.RefreshCount);
+//        }
+
+//        [TestMethod]
+//        public void Refresh_Children_OnlyChildrenRefreshed()
+//        {
+//            var parentComp = new TestComponent();
+//            var childComp = new TestComponent();
+//            var parent = new TestViewModel(parentComp);
+//            var child = new TestViewModel(childComp);
+//            parent = new TestViewModel(parentComp, new[] { child });
+//            parent.Refresh(ViewModelBase.Propagate.Children);
+//            Assert.AreEqual(0, parentComp.RefreshCount);
+//            Assert.AreEqual(1, childComp.RefreshCount);
+//        }
+
+//        [TestMethod]
+//        public void Refresh_Parents_OnlyParentsRefreshed()
+//        {
+//            var parentComp = new TestComponent();
+//            var childComp = new TestComponent();
+//            var parent = new TestViewModel(parentComp);
+//            var child = new TestViewModel(childComp, null, parent);
+//            child.Refresh(ViewModelBase.Propagate.Parents);
+//            Assert.AreEqual(1, parentComp.RefreshCount);
+//            Assert.AreEqual(0, childComp.RefreshCount);
+//        }
+
+//        [TestMethod]
+//        public void Refresh_WholeTree_AllRefreshed()
+//        {
+//            var rootComp = new TestComponent();
+//            var childComp = new TestComponent();
+//            var root = new TestViewModel(rootComp);
+//            var child = new TestViewModel(childComp, null, root);
+//            root = new TestViewModel(rootComp, new[] { child });
+//            child.Refresh(ViewModelBase.Propagate.WholeTree);
+//            Assert.AreEqual(1, rootComp.RefreshCount);
+//            Assert.AreEqual(1, childComp.RefreshCount);
+//        }
+//    }
+//}
