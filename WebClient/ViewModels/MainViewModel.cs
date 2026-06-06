5df7239917cecd947e808c92c53de37dbe7c07fa
Patryk PGR <1992grabek@gmail.com>
2026-06-06 21:51:04 +0200

    save work

--- WebClient/ViewModels/MainViewModel.cs
+++ WebClient/ViewModels/MainViewModel.cs
@@ -6,8 +6,18 @@ using WebClient.ViewModels;
 
 namespace WebClient.ViewModels
 {
+    record ExistingSheets
+    {
+        public SharedCode.SpendingFileType spendingFileType { get; set; }
+        public string Title { get; set; }
+        public string Id { get; set; }
+    }
+
     public class MainViewModel : ViewModelBase
     {
+        private HashSet<ExistingSheets> sheetPendingToCreate = new();
+        private List<ExistingSheets> sheets = new List<ExistingSheets>();
+
         public DataPickerViewModel DataPickerViewModel { get; set; }
         public Func<CostAnalizerApplication> CostAnalizerApp { get; }
         public Counter MainPage => (base.Component as Counter)!;
@@ -21,12 +31,37 @@ namespace WebClient.ViewModels
             DataPickerViewModel = new DataPickerViewModel(getDatPicker, mainApp);
             DataPickerViewModel.UserInputBeforeDataAdded += DataPickerViewModel_UserInputBeforeDataAdded;
             CostAnalizerApp = mainApp;
+            this.MainPage.BeforeSheetAdded += MainPage_BeforeSheetAdded;
+            this.MainPage.SheetAdded += MainPage_SheetAdded; ;
+        }
+
+        private void MainPage_SheetAdded(string obj)
+        {
+
+        }
+
+        private void MainPage_BeforeSheetAdded(string id)
+        {
         }
 
         private void DataPickerViewModel_UserInputBeforeDataAdded(SharedCode.SpendingFileType type)
         {
-            MainPage.AddPage(Counter.SheetGroup.Data, type.ToString());
+            var id = Guid.NewGuid().ToString();
+            var title = type.ToString();
+            sheetPendingToCreate.Add(new ExistingSheets { Id = id, spendingFileType = type, Title = title });
+
+            MainPage.AddSheet(Counter.SheetGroup.Data, title, id);
+
             base.Refresh();
+        }
+
+        public void UserInput_AddAnalyticAdded()
+        {
+
+        }
+
+        public void UserInput_AddDataClicked()
+        {
         }
     }
 }
