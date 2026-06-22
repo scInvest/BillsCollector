a8b6df46f52a1d99f7ed73ab9f9be79e8214154d
Patryk PGR <1992grabek@gmail.com>
2026-06-22 22:54:13 +0200

    Dodaj przycisk wysyłania zależny od stanu agenta

--- WebClient/Components/UIComponents/ChatAgentWindow/ChatAgentWindowMessageOptions.razor.cs
+++ WebClient/Components/UIComponents/ChatAgentWindow/ChatAgentWindowMessageOptions.razor.cs
@@ -1,9 +1,52 @@
 using Microsoft.AspNetCore.Components;
+using System.ComponentModel;
+using WebClient.Components.UIComponents.ChatAgent;
 
 namespace WebClient.Components.UIComponents.ChatAgentWindow;
 
-public partial class ChatAgentWindowMessageOptions : ComponentBase
+public partial class ChatAgentWindowMessageOptions : ComponentBase, IDisposable
 {
+    private ChatAgentViewModel? subscribedParent;
+
     [Parameter]
     public ChatAgentWindowMessageOptionsViewModel ViewModel { get; set; } = default!;
+
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+
+        if (ReferenceEquals(subscribedParent, ViewModel.ParentViewModel))
+        {
+            return;
+        }
+
+        if (subscribedParent is not null)
+        {
+            subscribedParent.PropertyChanged -= ParentViewModel_PropertyChanged;
+        }
+
+        subscribedParent = ViewModel.ParentViewModel;
+
+        if (subscribedParent is not null)
+        {
+            subscribedParent.PropertyChanged += ParentViewModel_PropertyChanged;
+        }
+    }
+
+    private void ParentViewModel_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName is null || e.PropertyName == nameof(ChatAgentViewModel.State))
+        {
+            _ = InvokeAsync(StateHasChanged);
+        }
+    }
+
+    public void Dispose()
+    {
+        if (subscribedParent is not null)
+        {
+            subscribedParent.PropertyChanged -= ParentViewModel_PropertyChanged;
+            subscribedParent = null;
+        }
+    }
 }
